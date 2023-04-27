2c857b3237c61eaf3275ab17de743fbf660c2b4b
Toma John Musa <tomajohn316@yahoo.com>
2023-04-27 12:57:20 +0100

    Add complete header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.svg";
+import iologo from "../assets/images/io.svg";
+
 const Header = () => (
-  <>head</>
+  <header className="container">
+    <div className="logo">
+      <Link to="/">
+        <img src={logo} alt="GDG logo" />
+      </Link>
+    </div>
+
+    <div className="io-logo">
+        <img src={iologo} alt="Google I/O logo" />
+    </div>
+
+    <div className="btn-div">
+      <Link to="*" className="btn">
+        Notify me
+      </Link>
+    </div>
+  </header>
 );
 
 export default Header;
