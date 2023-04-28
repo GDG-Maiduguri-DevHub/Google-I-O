b81c3490787f47ed02695ab952a56ce58113b5ff
Toma John Musa <tomajohn316@yahoo.com>
2023-04-28 22:13:34 +0100

    Implement counter functionality

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,49 +1,92 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const Counter = () => (
-  <section className="counter container">
-    <div className="clock">
-      <div className="day clock-item white-text">
-        <div className="boxes">
-          <div className="green">5</div>
-          <div className="green">6</div>
+const Counter = () => {
+  const eventDate = new Date("2023/06/24 10:00:00");
+  let [date, setDate] = useState({
+    days: "",
+    hours: "",
+    minutes: "",
+    seconds: "",
+  });
+
+
+  useEffect(() => {
+    const timeout = setInterval(() => {
+      let date = new Date();
+
+      let diff = Math.abs(eventDate - date);
+
+      // Calculate the number of days, hours, minutes, and seconds
+      const nodays = (Math.floor(diff / (1000 * 60 * 60 * 24))).toString();
+      diff -= parseInt(nodays) * (1000 * 60 * 60 * 24);
+
+      const hours = Math.floor(diff / (1000 * 60 * 60)).toString();
+      diff -= parseInt(hours) * (1000 * 60 * 60);
+
+      const minutes = Math.floor(diff / (1000 * 60)).toString();
+      diff -= parseInt(minutes) * (1000 * 60);
+
+      const seconds = Math.floor(diff / 1000).toString();
+
+      let newDate = {
+        days: (nodays < 10 ? "0" + nodays : nodays),
+        hours: (hours < 10 ? "0" + hours : hours),
+        minutes: (minutes < 10 ? "0" + minutes : minutes),
+        seconds: (seconds < 10 ? "0" + seconds : seconds)
+      }
+
+      setDate(newDate);
+
+    }, 1000);
+
+    return () => (clearInterval(timeout));
+  }, [])
+  return (
+    <section className="counter container">
+      <div className="clock">
+        <div className="day clock-item white-text">
+          <div className="boxes">
+            <div className="green">{date.days[0]}</div>
+            <div className="green">{date.days[1]}</div>
+          </div>
+          <p className="white-text small-text">Days</p>
         </div>
-        <p className="white-text small-text">Days</p>
+
+        <div className="day clock-item white-text">
+          <div className="boxes">
+            <div className="red">{date.hours[0]}</div>
+            <div className="red">{date.hours[1]}</div>
+          </div>
+          <p className="white-text small-text">Hours</p>
+        </div>
+
+        <div className="day clock-item white-text">
+          <div className="boxes">
+            <div className="blue">{date.minutes[0]}</div>
+            <div className="blue">{date.minutes[1]}</div>
+          </div>
+          <p className="white-text small-text">Minutes</p>
+        </div>
+
+        <div className="day clock-item white-text">
+          <div className="boxes">
+            <div className="yellow">{date.seconds[0]}</div>
+            <div className="yellow">{date.seconds[1]}</div>
+          </div>
+          <p className="white-text small-text">Seconds</p>
+        </div>
       </div>
 
-      <div className="day clock-item white-text">
-        <div className="boxes">
-          <div className="red">3</div>
-          <div className="red">3</div>
+      <div className="counter-act black-text">
+        <div className="center-left">
+          <h3>For Sponsorship</h3>
+          <p>Join to build the tech community</p>
+          <Link to="/" className="btn btn2">Sponsor Now!</Link>
         </div>
-        <p className="white-text small-text">Hours</p>
       </div>
-
-      <div className="day clock-item white-text">
-        <div className="boxes">
-          <div className="blue">1</div>
-          <div className="blue">2</div>
-        </div>
-        <p className="white-text small-text">Minutes</p>
-      </div>
-
-      <div className="day clock-item white-text">
-        <div className="boxes">
-          <div className="yellow">4</div>
-          <div className="yellow">0</div>
-        </div>
-        <p className="white-text small-text">Seconds</p>
-      </div>
-    </div>
-
-    <div className="counter-act black-text">
-      <div className="center-left">
-        <h3>For Sponsorship</h3>
-        <p>Join to build the tech community</p>
-        <Link to="/" className="btn btn2">Sponsor Now!</Link>
-      </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Counter;
